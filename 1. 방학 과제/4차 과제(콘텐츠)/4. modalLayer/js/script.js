42addbf9f82dfbcd104ccb4cfeac2979800d7a1a
HomeDesktop <ajy720@gmail.com>
2020-03-22 07:30:28 +0900

    모달 레이어

---
+++ 1. 방학 과제/4차 과제(콘텐츠)/4. modalLayer/js/script.js
@@ -0,0 +1,39 @@
+$(window).ready(function(){
+    $(".gnb").hover(function(){
+        $(".sub, .nav_bg").stop().slideDown()
+    }, function(){
+        $(".sub, .nav_bg").stop().slideUp()
+    })
+
+    var slide = $(".slide li")
+    var n = 0
+
+    function playSlide(){
+        $(slide[n]).animate({
+            left:"1200px"
+        }, 2000, function(){
+            $(this).css({left:"-1200px"});
+        })
+
+        n++
+
+        if(n > 2) n = 0
+
+        $(slide[n]).animate({left:"0"}, 2000)
+    }
+
+    playSlide()
+    var timer = setInterval(() => {
+        playSlide()
+        console.log("slide")
+    }, 4000);
+
+    $(".notice > .box > li:first-child > a").click(function(){
+        $(".popup_bg").show()
+    })
+
+    $(".btn").click(function(){
+        $(".popup_bg").hide()
+    })
+    
+})
