70ce786394f0a7dc704045e9c28872e56fb5bbd1
HomeDesktop <ajy720@gmail.com>
2020-03-22 04:07:58 +0900

    레이어 팝업

---
+++ 1. 방학 과제/4차 과제(콘텐츠)/3. LayerPopUp/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(()=>{
+    $("ul.gnb>li").hover(function(){
+        $("ul.sub").stop().slideDown()
+    }, function(){
+        $("ul.sub").stop().slideUp()
+    })
+
+    var slide = $(".slider li");
+    var n = 0;
+
+    function playSlide(){
+        $(slide[n]).animate({
+            top:"300px"
+        }, 1000, function(){
+            $(this).css({top:"-300px"});
+        })
+
+        n++;
+
+        if(n > 2) n = 0
+
+        $(slide[n]).animate({top:"0px"}, 1000);
+    }
+
+    var timer = setInterval(() => {
+        playSlide()
+    }, 2000);
+
+    $(".tab > h3").click(function(){
+        $(this).addClass("on")
+        $(this).siblings().removeClass("on")
+
+        var on = $(".box > .on")
+        $(on).removeClass("on")
+        $(on).siblings().addClass("on")
+    })
+
+    $(".notice > li:first-child > a").click(function(){
+        $(".popup").show()
+    })
+    
+    $(".btn").click(function(){
+        $(".popup").hide()
+    })
+
+
+})
