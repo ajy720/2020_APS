6f90e851fc0241acd52e0c602542e69d26929764
HomeDesktop <ajy720@gmail.com>
2020-03-23 23:26:40 +0900

    5차 과제

---
+++ 1. 방학 과제/5차 과제(하단부)/3. LogoMenu/js/script.js
@@ -0,0 +1,7 @@
+$(window).ready(function(){
+    $("ul.gnb > li").hover(function(){
+        $(this).find("ul.sub").stop().slideDown();
+    }, function(){
+        $(this).find("ul.sub").stop().slideUp();
+    })
+})
---
+++ 1. 방학 과제/5차 과제(하단부)/4. MenuFamily/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("ul.gnb>li").hover(function(){
+        $(this).find("ul.sub").stop().slideDown()
+        console.log("Down")
+    }, function(){
+        $(this).find("ul.sub").stop().slideUp()
+        console.log("UP")
+    })
+})
