c3ba9e34f6d04e5d34653726506a07df28f2da23
AnHyeonSeok <ajy720@gmail.com>
2020-06-26 13:42:33 +0900

    해운대 빛축제 ing

---
+++ 3. 개학 수업/8. 해운대 빛축제/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	$(".main > li").hover(
+		function () {
+			$(this).find(".sub").stop().slideDown();
+		},
+		function () {
+			$(this).find(".sub").stop().slideUp();
+		}
+	);
+
+	var s = $(".slider a");
+	var n = 0;
+
+	function playSlide() {
+		$(s[n++]).fadeOut("slow");
+
+    if (n > 2) n = 0;
+    
+    $(s[n]).fadeIn("slow");
+  }
+  
+  setInterval(playSlide, 3000);
+
+  $(".notice_gal h3.tab").click(function(){
+    $(".notice_gal h3").removeClass("on")
+    $(this).addClass("on")
+    $(".notice_gal .box").removeClass("on")
+    $(this).next(".box").addClass("on")
+  })
+});
