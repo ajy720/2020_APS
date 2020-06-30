9d012bcfa8c9751f64d3e150ccd40b8769680fe2
AHS <ajy720@gmail.com>
2020-06-30 11:28:00 +0900

    해운대 빛축제 & gitignore

--- 3. 개학 수업/8. 해운대 빛축제/js/script.js
+++ 3. 개학 수업/8. 해운대 빛축제/js/script.js
@@ -14,17 +14,27 @@ $(document).ready(function () {
 	function playSlide() {
 		$(s[n++]).fadeOut("slow");
 
-    if (n > 2) n = 0;
-    
-    $(s[n]).fadeIn("slow");
-  }
-  
-  setInterval(playSlide, 3000);
+		if (n > 2) n = 0;
 
-  $(".notice_gal h3.tab").click(function(){
-    $(".notice_gal h3").removeClass("on")
-    $(this).addClass("on")
-    $(".notice_gal .box").removeClass("on")
-    $(this).next(".box").addClass("on")
-  })
+		$(s[n]).fadeIn("slow");
+	}
+
+	setInterval(playSlide, 3000);
+
+	$(".notice_gal h3.tab").click(function () {
+		$(".notice_gal h3").removeClass("on");
+		$(this).addClass("on");
+		$(".notice_gal .box").removeClass("on");
+		$(this).next(".box").addClass("on");
+	});
+
+	$(".notice_gal li:first-child").click(function () {
+		$(".modal").stop().fadeIn();
+	});
+	$(".pp button").click(closeModal);
+	$(".modal .bg").click(closeModal);
+
+	function closeModal () {
+		$(".modal").stop().fadeOut();
+	}
 });
